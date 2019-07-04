6801230a16d91f607dd6530d028f318aa579839b
lars_martin <lars.martin.kleppe@vipps.no>
2019-07-04 16:32:37 +0200

    Fetches content from GitHub

--- src/index.js
+++ src/index.js
@@ -4,13 +4,33 @@ import { Sidebar } from './sidebar.js';
 import { ContentField } from './content.js';
 import './index.css';
 
+
 // The entire page is contained here
-const DocuPage = () => (
-    <section className="DocuPage">
-        <Sidebar/>
-        <ContentField/>
-    </section>
-)
+class DocuPage extends React.Component{
+
+    state = {fullText: ""}
+    componentDidMount = () => {
+        this.getContent();
+    }
+
+    getContent = () => {
+        const url = "https://raw.githubusercontent.com/vippsas/vipps-ecom-api/master/vipps-ecom-api.md"
+        fetch(url)
+        .then(response => 
+            response.text().then(rendered => this.setState({ fullText: rendered}))
+        )
+        .catch(error => console.log("Something went wrong..", error));
+    }
+
+    render() {
+        return (
+            <section className="DocuPage">
+                <Sidebar dataFelt={"Ett eller annet her"}/>
+                <ContentField/>
+            </section>
+        )
+    }
+}
 
 ReactDOM.render(
     <body>
