4594cb8e3381609a374533834a8d2c8175d51204
Lasse Vad <lasse.vad@lassevadvippsnos-MacBook-Pro.local>
2019-07-05 12:45:43 +0200

    Interactive sidebar nav with correct alignments

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,56 +1,94 @@
-import React from 'react';
+import React from "react";
+import {
+  SideNav,
+  SideNavItem,
+  Collapsible,
+  CollapsibleItem
+} from "react-materialize";
+import "./materialize.css";
+import M from "materialize-css";
 
 // Contains the menuitems and backlink
 export const Sidebar = () => (
-    <section className="Sidebar">
-        <SidebarHeader/>
-        <SidebarMenu/>
-    </section>
-)
-
+  <section className="Sidebar">
+    <SidebarHeader />
+    <SidebarMenu />
+  </section>
+);
 
 // Header for logo and backlink
 const SidebarHeader = () => (
-    <div className="SidebarHeader">
-        <img className="Logo" src="https://www.vipps.no/static/vipps_theme/1.0.31/media/extra-images/vipps-logo.svg" alt="logo"/>
-    </div>
-)
+  <div className="SidebarHeader">
+    <img
+      className="Logo logoMarg"
+      src="https://www.vipps.no/static/vipps_theme/1.0.31/media/extra-images/vipps-logo.svg"
+      alt="logo"
+    />
+  </div>
+);
 
 // Structures the sidebar content
 const SidebarMenu = () => (
-    <div className="SidebarMenu">
-        <SidebarNav />
-        <ApiLink />
-    </div>
-)
+  <div className="SidebarMenu">
+    <SidebarNav />
+    <ApiLink />
+  </div>
+);
 
 // Navigation Menu
 const SidebarNav = () => (
-    //TODO: List elements should be 
-    <div className="SidebarNav">
-        <ul className="NavListTop">
-            <li className="NavElemTop">Lorem</li>
-            <ul className="NavListBot">
-                <li className="NavElemBot">
-                    Ipsum
-                </li>
-                <li className="NavElemBot">
-                    Dolor
-                </li>
-                <li className="NavElemBot">
-                    Sit Amet
-                </li>
-            </ul>
-            <li className="NavElemTop">Ipsum</li>
-            <li className="NavElemTop">Dolor</li>
-            <li className="NavElemTop">Sit</li>
-            <li className="NavElemTop">Amet</li>
-            <li className="NavElemTop">Ferdig</li>
-        </ul>
-    </div>
-)
+  //TODO: List elements should be
+  <div>
+    <SideNav class="sidebarMarg">
+      <Collapsible>
+        <CollapsibleItem header="Introduction">
+          <li>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+          </li>
+        </CollapsibleItem>
+        <CollapsibleItem header="Payment types">
+          <li>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+          </li>
+        </CollapsibleItem>
+        <CollapsibleItem header="Webstore payments">
+          <li>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+          </li>
+        </CollapsibleItem>
+        <CollapsibleItem header="Webstore payments">
+          <li>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+          </li>
+        </CollapsibleItem>
+        <CollapsibleItem header="Webstore payments">
+          <li>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+          </li>
+        </CollapsibleItem>
+        <CollapsibleItem header="Webstore payments">
+          <li>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+            <ul class="listEl">test</ul>
+          </li>
+        </CollapsibleItem>
+      </Collapsible>
+    </SideNav>
+  </div>
+);
 
 // Links to full api doc
 const ApiLink = () => (
-    <button className="ApiLink">Full API Documentation</button>
-)
+  <button className="ApiLink">Full API Documentation</button>
+);
