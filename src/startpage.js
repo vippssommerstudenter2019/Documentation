d8eeea3d164782185eb2b2afa3b8ef9790d11340
Mats Jørgen Skaslien <matsjorgen@hotmail.com>
2019-07-08 11:32:06 +0200

    Adds Router links to cards and api button

--- src/startpage.js
+++ src/startpage.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export const DocCard = (props) => (
     <div className="DocCard">
         <img src={props.img.src} alt={props.img.alt} width="80%"/>
         <h2>{props.title}</h2>
         <p>{props.text}</p>
-        <div className="DocCardButtons">
-            <button className="GetStartedButton"  onClick={() => props.startClick(props.startLink)}>Get Started</button>
-            <button className="DocumentationLink"  onClick={() => props.docuClick(props.docuLink)}>Documentation</button>
+        <div className="DocCardLinks">
+            <Link className="GetStartedLink"  to={props.startLink}>Get Started</Link>
+            <Link className="DocumentationLink"  to={props.docLink}>Documentation</Link>
         </div>
     </div>
 )
@@ -15,6 +16,6 @@ export const DocCard = (props) => (
 export const ApiDoc = (props) => (
     <div className="ApiDoc">
         <p>or go straight to...</p>
-        <button className="FullAPIButton" onClick={ () => props.apiClick}>Full API documentation</button>
+        <Link to={props.apiLink} className="FullAPILink" >Full API Documentation</Link>
     </div>
 )
