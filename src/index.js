cdd25fa4e3d81aa2131c75b7efec24653d106f6e
Mats Jørgen Skaslien <matsjorgen@hotmail.com>
2019-07-09 15:33:35 +0200

    Adds logo to startpage

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import "./components/sidebar/materialize.css";
 import M from "materialize-css";
 import "./components/sidebar/sidebar.css";
 import { Link } from "react-router-dom";
+import vipps_dev from "./img/vipps_dev.svg"
 
 // TODO: startpath should be "/documentation/" and not "/"
 const StartPage = () => (
@@ -32,33 +33,36 @@ const StartPage = () => (
 
 const Cards = () => (
     <div className="StartPage">
-            <div className="Cards">
-                <DocCard img={{src:"https://www.vipps.no/media/images/ta_betalt_pa_nett.max-320x320.jpegquality-60.png", alt:"Ta betalt på nett"}}
-                        title="eCommerce"
-                        text="Get Vipps checkout on your webstore"
-                        startLink="/"
-                        docLink="/documentation/ecommerce/"
-                        docName="ecom"
-                />
-                <DocCard img={{src:"https://www.vipps.no/media/images/sende_regninger.max-320x320.jpegquality-60.png", alt:"Send regninger"}}
-                        title="Invoice"
-                        text="Send invoices with Vipps"
-                        startLink="/"
-                        docLink="/documentation/invoice/"
-                        docName="invoice"
-                />
-                <DocCard img={{src:"https://www.vipps.no/media/images/vipps_logginn.max-320x320.jpegquality-60.png", alt:"Logg inn"}}
-                        title="Login"
-                        text="Secure login and identification with Vipps"
-                        startLink="/"
-                        docLink="/documentation/secure-login/"
-                        docName="login"
-                />
-            </div>
-            {/* <div className="APIcontainer">
-                <ApiDoc apiLink="/"/>
-            </div> */}
+        <div className="VippsDev">
+            <img src={vipps_dev} alt="Vipps Developers"/>
         </div>
+        <div className="Cards">
+            <DocCard img={{src:"https://www.vipps.no/media/images/ta_betalt_pa_nett.max-320x320.jpegquality-60.png", alt:"Ta betalt på nett"}}
+                    title="eCommerce"
+                    text="Get Vipps checkout on your webstore"
+                    startLink="/"
+                    docLink="/documentation/ecommerce/"
+                    docName="ecom"
+            />
+            <DocCard img={{src:"https://www.vipps.no/media/images/sende_regninger.max-320x320.jpegquality-60.png", alt:"Send regninger"}}
+                    title="Invoice"
+                    text="Send invoices with Vipps"
+                    startLink="/"
+                    docLink="/documentation/invoice/"
+                    docName="invoice"
+            />
+            <DocCard img={{src:"https://www.vipps.no/media/images/vipps_logginn.max-320x320.jpegquality-60.png", alt:"Logg inn"}}
+                    title="Login"
+                    text="Secure login and identification with Vipps"
+                    startLink="/"
+                    docLink="/documentation/secure-login/"
+                    docName="login"
+            />
+        </div>
+        {/* <div className="APIcontainer">
+            <ApiDoc apiLink="/"/>
+        </div> */}
+    </div>
 )
 
 // The entire page is contained here
