66beade8b9ae555a2827b2c0a8c13dd6705fa429
simengangstad <simen.gangstad@me.com>
2019-07-10 16:53:30 +0200

    Change layout

--- src/components/howitworks/IntroBox.js
+++ src/components/howitworks/IntroBox.js
@@ -15,20 +15,18 @@ class IntroBox extends React.Component {
 
             <div className="intro-wrapper">
                 <div className="intro-text" >
-                    <div className="large-font-size">
-                        {this.props.content.subtitle}
-                    </div>
                     <div className="xxlarge-font-size">
                         <div className="intro-title">
                             {this.props.content.title}
                         </div>
+                    <div className="xlarge-font-size">
+                        {this.props.content.subtitle}
                     </div>
-                    <div className="large-font-size">
+                    <br/>
+                    </div>
+                    <div className="default-font-size">
                         {this.props.content.description} 
                     </div>
-                </div>
-                <div className="intro-image">
-                    <img src={this.props.content.imgPath} alt="Woman Using ecom"/>
                 </div>
             </div>
         );
