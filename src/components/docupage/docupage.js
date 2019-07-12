267b28fbcfca77e019bc161b465bd1049cd861c3
Mats Jørgen Skaslien <matsjorgen@hotmail.com>
2019-07-12 13:15:19 +0200

    Refactors DocuPage

--- src/components/docupage/docupage.js
+++ src/components/docupage/docupage.js
@@ -3,66 +3,30 @@ import ReactMarkdown from "react-markdown";
 import HeadingRenderer from "./HeadingRenderer.js";
 import CodeBlock from "./CodeBlock.js";
 import Sidebar from "../sidebar/sidebar.js";
+import DeveloperResources from './DeveloperResources'
+import { SOURCE_URLS,
+          DEV_URLS,
+          PAGE_TITLES,
+          DEV_SIDEBAR_HEADER } from './Constants.js'
 import "./../../styles/index.css";
 import "./../../styles/vipps-style.css";
 import "./docupage.css";
-import { formatDescriptionToIncludeHoverLinks } from "../howitworks/Step";
 
 class DocuPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.urls = {
-      ecom:
-        "https://raw.githubusercontent.com/vippsas/vipps-ecom-api/master/vipps-ecom-api.md",
-      login:
-        "https://raw.githubusercontent.com/vippsas/vipps-login-api/master/vipps-login-api.md",
-      invoice:
-        "https://raw.githubusercontent.com/vippsas/vipps-invoice-api/master/vipps-invoice-api.md"
-    };
-    this.devResUrls = {
-      ecom: [
-        [
-          "Postman",
-          "https://github.com/vippsas/vipps-ecom-api/tree/master/tools"
-        ],
-        [
-          "FAQ",
-          "https://github.com/vippsas/vipps-ecom-api/blob/master/vipps-ecom-api-faq.md"
-        ],
-        ["Swagger", "https://vippsas.github.io/vipps-ecom-api/"]
-      ],
-      login: [
-        [
-          "Postman",
-          "https://github.com/vippsas/vipps-login-api/tree/master/tools"
-        ],
-        ["FAQ", "#faq"],
-        ["Swagger", "https://vippsas.github.io/vipps-login-api/"]
-      ],
-      invoice: [
-        [
-          "Postman",
-          "https://github.com/vippsas/vipps-invoice-api/tree/master/tools"
-        ],
-        ["FAQ", "#faq"],
-        ["Swagger ISP", "https://vippsas.github.io/vipps-invoice-api/isp.html"],
-        ["Swagger IPP", "https://vippsas.github.io/vipps-invoice-api/ipp.html"]
-      ]
-    };
-    this.pageTitles = {
-      ecom: "Vipps eCommerce API",
-      login: "Vipps Login API",
-      invoice: "Vipps Invoice API"
-    };
-
-    this.state = {
-      content: "",
-      headers: []
-    };
+  state = {
+    content: "",
+    headers: []
   }
 
+  srcURL = SOURCE_URLS[this.props.doc];
+  devURLs = DEV_URLS[this.props.doc];
+  pageTitle = PAGE_TITLES[this.props.doc];
+  devResourceHeader = DEV_SIDEBAR_HEADER;
+
+
+
   componentDidMount() {
-    this.getContent().then(response =>
+    fetch(this.srcURL).then(response =>
       response.text().then(fullText => 
         this.displayContent(fullText).then(
           this.goToAnchor
@@ -70,13 +34,8 @@ class DocuPage extends React.Component {
     );
   }
 
-  // Fetches raw content from Github and puts it in the DocuPage state
-  getContent() {
-    return fetch(this.urls[this.props.doc]);
-  }
-
   async displayContent(fullText) {
-    const content = this.filterMarkdown(fullText, this.props.doc);
+    const content = this.filterMarkdown(fullText);
     const headers = this.getHeaders(content);
     this.setState({
       content: content,
@@ -110,21 +69,11 @@ class DocuPage extends React.Component {
 
   // Return one or two swagger subheaders
   getChildren() {
-    let childs = [];
-    this.devResUrls[this.props.doc].forEach(header => {
-      childs.push({ name: header[0], anchor: header[1] });
+    let children = [];
+    this.devURLs.forEach(header => {
+      children.push({ name: header[0], anchor: header[1] });
     });
-    return childs;
-  }
-
-  // Because of design issues, we add our own header and subheaders to the sidebar
-  addSpecialHeader() {
-    let devRes = {
-      name: "Developer resources",
-      anchor: "#developer-resources",
-      children: this.getChildren()
-    };
-    return devRes;
+    return children;
   }
 
   // Filters the content fetched from Github into headers
@@ -153,20 +102,18 @@ class DocuPage extends React.Component {
       navbarHeaders[2].name === "Table of contents"
         ? navbarHeaders.slice(3)
         : navbarHeaders.slice(2);
-    sidebarHeaders.unshift(this.addSpecialHeader());
+    sidebarHeaders.unshift(this.devResourceHeader(this.getChildren()));
     return sidebarHeaders;
   }
 
   // Filters out table of content and first heading + paragraph
-  filterMarkdown(text, doc) {
-    let lines = text.split("\n");
+  filterMarkdown(text) {
     let filtered_markdown = "";
     let filter_out = false;
-    for (let line of lines) {
+    for (let line of text.split("\n")) {
       if (
-        (line.startsWith("# ") &&
-          line.toLowerCase().includes(this.pageTitles[doc].toLowerCase())) ||
-        line.toLowerCase().includes("table of content")
+        (line.startsWith("# ") && line.toLowerCase().includes(this.pageTitle.toLowerCase())) ||
+          line.toLowerCase().includes("table of content")
       ) {
         filter_out = true;
       } else if (filter_out === true && line.startsWith("#")) {
@@ -176,45 +123,18 @@ class DocuPage extends React.Component {
         filtered_markdown += line.toString() + "\n";
       }
     }
-    // this.setState(
-    //   {
-    //     editedText: filtered_markdown
-    //   },
-    //   this.getHeaders
-    // );
     return filtered_markdown;
   }
 
-  resourceSection = doc => (
-    <div className="devResources">
-      <h2>Developer resources</h2>
-      <hr />
-      {this.devResUrls[doc].map(element => (
-        <div>
-          <a href={element[1]}>
-            <div>
-              {element[0]}
-              <p>{String.fromCharCode(10132)}</p>
-            </div>
-          </a>
-          <hr />
-        </div>
-      ))}
-    </div>
-  );
 
-  render() {
-    const { doc } = this.props;
-    return (
+
+  render = () => (
       <div className="Container">
         <div className="Sidebar">
-          <Sidebar headers={this.state.headers} api={doc} />
+          <Sidebar headers={this.state.headers} api={this.props.doc} />
         </div>
         <div>
-          <h1 id="developer-resources">
-            Documentation - {this.pageTitles[doc]}
-          </h1>
-          {this.resourceSection(doc)}
+          <DeveloperResources devURLs={this.devURLs} pageTitle={this.pageTitle}/>
           <div className="Content">
             <ReactMarkdown
               source={this.state.content}
@@ -224,7 +144,6 @@ class DocuPage extends React.Component {
         </div>
       </div>
     );
-  }
 }
 
 export default DocuPage;
