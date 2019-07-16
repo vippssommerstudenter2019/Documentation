42a1e4f61ddc54d05665fd7dcb5fdc02b8aa3581
simengangstad <simen.gangstad@me.com>
2019-07-16 12:13:00 +0200

    Change swagger extracter to const variable

--- src/components/howitworks/Content.js
+++ src/components/howitworks/Content.js
@@ -12,7 +12,7 @@ class Content extends React.Component {
 
     getDataFromSwagger(endpoint, swaggerData)  {
 
-        let swaggerExtracter = new SwaggerExtracter();
+        const swaggerExtracter = new SwaggerExtracter();
         let header = {}, body = {}, responses = {};
 
         // Check out if the swagger file contains the id (which is the endpoint)
