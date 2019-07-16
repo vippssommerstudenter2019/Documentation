6624a09023622ce48e5c3e3402ad9092d481b569
simengangstad <simen.gangstad@me.com>
2019-07-16 09:17:53 +0200

    Use new path for step

--- src/components/howitworks/Content.js
+++ src/components/howitworks/Content.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {Step} from "./Step";
+import {Step} from "../step/Step";
 import SwaggerExtracter from "../../model/SwaggerExtracter"
 
 const propTypes = {
