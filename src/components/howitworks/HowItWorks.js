3c96c8975a27897f2c4239a3d354e6fd60179d71
simengangstad <simen.gangstad@me.com>
2019-07-16 10:18:30 +0200

    Remove sticky container

--- src/components/howitworks/HowItWorks.js
+++ src/components/howitworks/HowItWorks.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Content from "./Content"
-import { StickyContainer } from 'react-sticky';
 import IntroBox from "./IntroBox";
 import OutroBox from "./OutroBox";
 import PropTypes from "prop-types";
