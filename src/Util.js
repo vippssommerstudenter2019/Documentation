81476fe19c6ddff73a1e25683b4250552f9fa41e
simengangstad <simen.gangstad@me.com>
2019-07-17 18:06:55 +0200

    Add hash function to utl

--- src/Util.js
+++ src/Util.js
@@ -1,3 +1,4 @@
+
 /**
  * Used to check wether an object is empty ({}).
  * 
@@ -6,3 +7,21 @@
 export function objectIsEmpty(object) {
     return Object.keys(object).length === 0 && object.constructor === Object;
 }
+
+/**
+ * Generates a hash (unique number) from a string.
+ */
+export function getHashCodeFromString(string) {
+
+    var hash = 0, i, chr;
+
+    if (string.length === 0) return hash;
+
+    for (i = 0; i < string.length; i++) {
+        chr = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        // Convert to 32bit integer
+        hash |= 0;
+    }
+    return hash;
+};
