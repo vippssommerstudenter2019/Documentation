4df0b61d2c4ac2408877649ca8c447528a110a79
simengangstad <simen.gangstad@me.com>
2019-07-22 13:47:45 +0200

    Generate example data based on spec

--- src/components/howitworks/Content.js
+++ src/components/howitworks/Content.js
@@ -2,12 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import Step from "./step/Step";
 import SwaggerExtracter from "../../model/SwaggerExtracter"
+import OpenAPIExtracter from "../../model/OpenAPIExtracter";
 
 const propTypes = {
     sections: PropTypes.object.isRequired,
     swaggerData: PropTypes.object.isRequired
 };
 
+const openAPIExtracter = new OpenAPIExtracter();
 const swaggerExtracter = new SwaggerExtracter();
 
 class Content extends React.Component {
@@ -19,13 +21,22 @@ class Content extends React.Component {
         subsections.push(
             <div key={title} className="hero-font-size section-title">{title}</div>
         )
-
+        
         for (const [id, subsection] of Object.entries(section)) {
             // We use the swagger extracter to get example headers, bodies and responses for every endpoint in this step.
 
             var endpointData = {};
             for (const endpoint of subsection.endpoints) {
-                const [header, body, responses] = swaggerExtracter.getExampleData(endpoint, this.props.swaggerData);
+
+                let header, body, responses;
+
+                if (this.props.swaggerData.hasOwnProperty("openapi")) {
+                    [header, body, responses] = openAPIExtracter.getExampleData(endpoint, this.props.swaggerData);
+                }
+                else if (this.props.swaggerData.hasOwnProperty("swagger")) {
+                    [header, body, responses] = swaggerExtracter.getExampleData(endpoint, subsection.modes[endpoint], this.props.swaggerData);
+                }
+
                 endpointData[endpoint] = {
                     header: header,
                     body: body,
