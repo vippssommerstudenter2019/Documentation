89d07dc34a1d23799aa1fdaa0b43d19265c52107
simengangstad <simen.gangstad@me.com>
2019-08-09 15:08:11 +0200

    Remove spacing

--- src/components/howitworks/IntroBox.js
+++ src/components/howitworks/IntroBox.js
@@ -4,28 +4,24 @@ import React from 'react';
  * Represents a box with a short overview over the content.
  */
 class IntroBox extends React.Component {
-    render() {
-        return (
-			<div className="intro-wrapper">
-
-					<div className="first-headline vippsorange">
-						{this.props.content.title}
-					</div>
-
-					<div className="preamble">
-						{this.props.content.introduction}
-					</div>
-
-					<div className="body-text p-spacing">
-						{this.props.content.description} 
-					</div>
-
-					<div className="body-text p-spacing">
-						{this.props.content.descriptiontwo} 
-					</div>
-			</div>
-        );
-	}
-};
+  render() {
+    return (
+      <div className="intro-wrapper">
+        <div className="first-headline vippsorange">
+          {this.props.content.title}
+        </div>
+        <div className="preamble">
+          {this.props.content.introduction}
+        </div>
+        <div className="body-text p-spacing">
+          {this.props.content.description}
+        </div>
+        <div className="body-text p-spacing">
+          {this.props.content.descriptiontwo}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default IntroBox;
